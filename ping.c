d033b97d8d4e54068f5ff3e1a349963f290847e7
pranav <pranavsuresh107@gmail.com>
2025-12-02 16:53:42 +0530

    somehow managed to get out of segmentation hell

--- ping.c
+++ ping.c
@@ -4,21 +4,40 @@
 int ip_addr_finding(){
 	//trying to ping all device in subnet
 	//ip -o -4 addr show scope global | awk '{print $4}' | wc -l  too show only ip address of interfaces with mask attached
-	FILE *f = ("ip -o -4 addr show scope global | awk '{print $4}' | wc -l","r");
+	//TODO: replace the above command with acutal func to check all ip addresses
+	FILE *f;
+	char number[64];
+	char ips[atoi(number)][16];
+
+	f = popen("ip -o -4 addr show scope global | awk '{print $4}' | grep \"/24\" | wc -l","r");
 	if (!f){
 		perror("command not working for finding ips");
 		return -1;
 	}
 
-	//saving output to variable number
-	char number[64];
 	//checking if variable number is empty
 	if (fgets(number,sizeof(number),f) == NULL){
-		perror("No output recieved from command");
+		perror("failed to get ips");
 		return -1;
 	}
 
 	//closing the pipe opened by *f
+	pclose(f);
+
+
+
+	//TODO: only currently working wiht /24 networks need to work on that
+	f = popen("ip -o -4 addr show scope global | awk '{print $4}' | grep \"/24\" | cut -d/ -f1","r");
+	if (!f){
+		perror("command not working for finding ips");
+		return -1;
+	}
+
+
+	//testing
+	char temp[50];
+	fgets(temp,sizeof(temp),f);
+	printf("%s\n",temp);
 	pclose(f);
 	
 	//convert ascii to integer
@@ -27,6 +46,6 @@ int ip_addr_finding(){
 
 int main(){
 	int numberofip = ip_addr_finding();
-	printf("%d\n",numberofip);
+	printf("no of ips:%d\n",numberofip);
 	return 0;
 }
