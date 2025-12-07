f739a3ca703060ce3f0af6ff9d61935390c1f4c4
pranav <pranavsuresh107@gmail.com>
2025-12-07 12:45:21 +0530

    made the qr to link to the local ip addr of system

--- html/index.php
+++ html/index.php
@@ -11,6 +11,10 @@
 <body>
 	<p>Scan the qr code on moobile</p>
 	<img src="qr.php" alt="QR code">
+	<?php
+	$cmd = "ip -o -4 addr show $interface 2>/dev/null | awk '{print $4}' | cut -d/ -f1 | grep \"192\"";
+	echo trim(shell_exec($cmd));
+	?>
 </body>
 
 </html>
--- html/qr.php
+++ html/qr.php
@@ -1,4 +1,5 @@
 <?php
 include "phpqrcode/qrlib.php";
-QRcode::png("HELO");
+$cmd = "ip -o -4 addr show $interface 2>/dev/null | awk '{print $4}' | cut -d/ -f1 | grep \"192\"";
+QRcode::png(trim(shell_exec($cmd)));
 ?>
